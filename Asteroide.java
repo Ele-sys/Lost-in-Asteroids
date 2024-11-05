c99b511b4a87d207dc0dbfe2ebabf156089aa250
Ele-sys <elenachiara01@gmail.com>
2024-11-05 16:10:10 +0100

    aggiungo la riga 10 e 11

--- Asteroide.java
+++ Asteroide.java
@@ -7,5 +7,7 @@ public class Asteroide {
 	public static void main(String args[]) {
         
         System.out.print("è una prova per usare gitHub desktop");
+        System.out.println("modifico il file");
+        System.out.println("ciaooo");
     }
 }
